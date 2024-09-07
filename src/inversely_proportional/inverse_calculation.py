13b05154478bf51a1134a966b3679f165f5f560d
Gabriel Mira <gllimamira@gmail.com>
2024-09-07 19:19:23 -0300

    calculo_do_inverssamente_proporcional_finalizado

--- src/inversely_proportional/inverse_calculation.py
+++ src/inversely_proportional/inverse_calculation.py
@@ -1,6 +1,31 @@
-def valida_valor(numbers: list) -> list:
-    for number in numbers:
+def valida_valor(numbers_list: list) -> list:
+    """valida os numeros que recebo do front
+
+    Args:
+        numbers (list): lista dos numeros que serao usados para os calculos
+
+    Returns:
+        list: se os valores digitados forem somente numeros
+        ele deixa inicia o codigo
+    """
+    for number in numbers_list:
         try:
             isinstance(number, float)
+            return True
         except:
-            ValueError
+            teste = ValueError
+            return teste
+
+
+def calculate_inverse_rule_of_three(numbers_list: list) -> list:
+
+    validate_number = valida_valor(numbers_list)
+
+    if not validate_number:
+        return validate_number
+
+    number_1, number_2, number_3 = numbers_list
+
+    result = (number_1 * number_2) / number_3
+
+    return result
